6397f3f190c254a8d4a21d4b73bb4d32f6429ded
annoinspace <aneesahalmask@gmail.com>
2024-01-01 18:19:49 +0000

    initial commit

---
+++ index.js
@@ -0,0 +1,31 @@
+import axios from "axios"
+import express from "express"
+import cheerio from "cheerio"
+
+const app = express()
+const port = process.env.PORT || 8000
+
+const url = "https://mydramalist.com/shows/top"
+
+axios
+  .get(url)
+  .then((response) => {
+    // Handle response
+    const html = response.data
+    const $ = cheerio.load(html)
+    let dramas = []
+    // Example: Selecting elements with a specific class
+    $(".text-primary.title").each(function () {
+      const title = $(this).text()
+      dramas.push(title)
+      console.log(title)
+    })
+  })
+  .catch((error) => {
+    // Handle error
+    console.log(error)
+  })
+
+app.listen(port, () => {
+  console.log("server running on port ", port)
+})
