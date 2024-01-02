018135e39c7baf7d938f5e806a88787b728b21ae
annoinspace <aneesahalmask@gmail.com>
2024-01-02 13:09:27 +0000

    only scraping the korean dramas

--- index.js
+++ index.js
@@ -1,30 +1,66 @@
+import "dotenv/config"
 import axios from "axios"
 import express from "express"
 import cheerio from "cheerio"
 
 const app = express()
-const port = process.env.PORT || 8000
+const port = process.env.PORT
 
-const url = process.env.URL_1
+const url = process.env.URL_ONE
 
-axios
-  .get(url)
-  .then((response) => {
-    // Handle response
+// axios
+//   .get(url)
+//   .then((response) => {
+//     // Handle response
+//     const html = response.data
+//     const $ = cheerio.load(html)
+//     let dramas = []
+//     // Example: Selecting elements with a specific class
+//     $(".text-primary.title").each(function () {
+//       const title = $(this).text()
+//       dramas.push(title)
+//       console.log(title)
+//     })
+//   })
+//   .catch((error) => {
+//     // Handle error
+//     console.log(error)
+//   })
+
+const scrapePage = async (pageNumber) => {
+  const urlToScrape = `${url}/shows/top?page=${pageNumber}`
+  try {
+    const response = await axios.get(urlToScrape)
     const html = response.data
     const $ = cheerio.load(html)
     let dramas = []
-    // Example: Selecting elements with a specific class
-    $(".text-primary.title").each(function () {
-      const title = $(this).text()
-      dramas.push(title)
-      console.log(title)
+
+    $(".box").each(function () {
+      const titleElement = $(this).find(".text-primary.title")
+      const infoSpan = $(this).find("span.text-muted").text()
+
+      if (infoSpan.includes("Korean")) {
+        const title = titleElement.text().trim()
+        dramas.push(title)
+      }
     })
-  })
-  .catch((error) => {
-    // Handle error
-    console.log(error)
-  })
+
+    // dramas now contains titles of Korean dramas
+    console.log(dramas)
+  } catch (error) {
+    console.error(`Error scraping page ${pageNumber}:`, error)
+  }
+}
+
+const main = async () => {
+  const totalPages = 2
+  for (let page = 1; page <= totalPages; page++) {
+    await scrapePage(page)
+    // Consider adding a delay here to reduce server load
+  }
+}
+
+main()
 
 app.listen(port, () => {
   console.log("server running on port ", port)
